b79e6acec53211d5c7c3e0d0f9130f84d0840452
jeebiz <hnxyhcwdl1003@163.com>
2018-05-20 16:34:29 +0800

    调整版本

--- first-bytebuddy/src/main/java/bytebuddy/MyAgent.java
+++ first-bytebuddy/src/main/java/bytebuddy/MyAgent.java
@@ -4,6 +4,7 @@ import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.DynamicType.Builder;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.utility.JavaModule;
@@ -16,28 +17,50 @@ public class MyAgent {
         System.out.println("this is an perform monitor agent.");
 
         AgentBuilder.Transformer transformer = new AgentBuilder.Transformer() {
-            @Override
-            public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder,
-                                                    TypeDescription typeDescription,
-                                                    ClassLoader classLoader) {
-                return builder
+
+			@Override
+			public Builder<?> transform(Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader,
+					JavaModule module) {
+				return builder
                         .method(ElementMatchers.<MethodDescription>any()) // 拦截任意方法
                         .intercept(MethodDelegation.to(TimeInterceptor.class)); // 委托
-            }
+			}
         };
 
         AgentBuilder.Listener listener = new AgentBuilder.Listener() {
-            @Override
-            public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, DynamicType dynamicType) {}
+          
+			@Override
+			public void onDiscovery(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+				// TODO Auto-generated method stub
+				
+			}
 
-            @Override
-            public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module) { }
+			@Override
+			public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module,
+					boolean loaded, DynamicType dynamicType) {
+				// TODO Auto-generated method stub
+				
+			}
 
-            @Override
-            public void onError(String typeName, ClassLoader classLoader, JavaModule module, Throwable throwable) { }
+			@Override
+			public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module,
+					boolean loaded) {
+				// TODO Auto-generated method stub
+				
+			}
 
-            @Override
-            public void onComplete(String typeName, ClassLoader classLoader, JavaModule module) { }
+			@Override
+			public void onError(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded,
+					Throwable throwable) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onComplete(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+				// TODO Auto-generated method stub
+				
+			}
         };
 
         new AgentBuilder
